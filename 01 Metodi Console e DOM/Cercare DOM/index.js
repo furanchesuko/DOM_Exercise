17b0f5195d62f52d6589ae6558bcf900850b73f1
Francesco <ballerifrancesco@gmail.com>
2024-02-29 11:53:00 +0100

    corrected exercises 2/3

---
+++ 01 Metodi Console e DOM/Cercare DOM/index.js
@@ -0,0 +1,9 @@
+/* Cerca tutti gli elementi con classe "paragrafo". */
+const paragraph = document.querySelectorAll(".paragrafo");
+console.log(paragraph);
+/* Cerca l' elemento con id "elemento-3" */
+const elementThree = document.querySelector("#elemento-3");
+console.log(elementThree);
+/* Cerca tutti gli elementi h2 della pagina */
+const h2Titles = document.querySelectorAll("h2");
+console.log(h2Titles);
